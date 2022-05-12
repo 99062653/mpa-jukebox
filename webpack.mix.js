8181faeff2c9314f3b03fc615bf47413ba2bbc94
99062653 <99062653@mydavinci.nl>
2022-05-12 14:22:10 +0200

    v0.20: Mix!
    added mix
    
    npm run dev to compile it

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,8 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+//npm run dev om hem te compilen
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+    .postCss('resources/css/app.css', 'public/css', [ //hoi wat dit doet: haalt app.css uit resources en zet hem naar public
         //
     ]);
