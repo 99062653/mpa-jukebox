856b25e56981030b8be07d59603dd544be2dc7bf
99062653 <99062653@mydavinci.nl>
2022-05-16 00:46:26 +0200

    v0.35.5: layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="{{ asset('/css/normalize.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/home.css') }}" rel="stylesheet">
-</head>
+@include("layout/header")
 
 <body class="antialiased">
-    <nav class="navbar fixed-top navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Fixed top</a>
-            <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    </nav>
-
+    @include("layout/navigation")
+    
+    @include("layout/footer")
 </body>
 
 </html>
---
+++ resources/views/layout/navigation.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar fixed-top">
+	<div class="container-fluid">
+		<a class="navbar-brand">
+			<img src="../img/logo.gif" alt="JukeBox_logo" width="250">
+		</a>
+		<div class="spinner-border" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	</div>
+</nav>
