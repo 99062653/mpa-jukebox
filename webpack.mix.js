f28f303509a336b38f12f0ccf6c68f155c603b77
99062653 <99062653@mydavinci.nl>
2022-05-15 23:41:03 +0200

    v0.25: bootstrap!

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
-//npm run dev om hem te compilen
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [ //hoi wat dit doet: haalt app.css uit resources en zet hem naar public
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
