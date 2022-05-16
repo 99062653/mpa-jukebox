4a7c2aee59d885c457111fad4d1efa18fdd23d68
99062653 <99062653@mydavinci.nl>
2022-05-16 02:21:28 +0200

    v0.37

---
+++ resources/views/functionally/time.blade.php
@@ -0,0 +1,27 @@
+<?php
+
+use Carbon\Carbon;
+
+?>
+@switch($t = date("h", strtotime(Carbon::now())))
+            @case($t > 06 && $t < 12)
+            <h1>Goedemorgen</h1>
+            @break
+
+            @case($t > 12 && $t < 18)
+            <h1>Goedemiddag,</h1>
+            @break
+
+            @case($t > 18 && $t > 24)
+            <h1>Goedeavond,</h1>
+            @break
+
+            @case($t > 00 && $t < 06)
+            <h1>Goedenacht,</h1>
+            @break
+
+            @default
+            <h1>Welkom,</h1>
+            @break
+
+        @endswitch
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,33 +2,12 @@
 <?php
 
 use Carbon\Carbon;
-date_default_timezone_set('Europe/Amsterdam')
 
 ?>
 <body>
     @include("layout/navigation")
     <div id="content">
-        @switch($t = date("h", strtotime(Carbon::now())))
-            @case($t > 06 && $t < 12)
-            <h1>Goedemorgen!</h1>
-            @break
-
-            @case($t > 12 && $t < 18)
-            <h1>Goedemiddag!</h1>
-            @break
-
-            @case($t > 18 && $t > 24)
-            <h1>Goedeavond!</h1>
-            @break
-
-            @case($t > 00 && $t < 06)
-            <h1>Goedenacht!</h1>
-            @break
-
-            @default
-            <h1>Welkom!</h1>
-        @endswitch
-        <p>het is nu <?= $t ?></p>
+        @include("functionally/time")
     </div>
 
     @include("layout/footer")
