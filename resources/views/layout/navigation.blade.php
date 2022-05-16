c3c3e4b4dea07afdb4d266822e7e5d39d57b7c15
99062653 <99062653@mydavinci.nl>
2022-05-16 10:56:02 +0200

    v0.39.5: the basics done

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -1,14 +1,14 @@
 <?php
 
-
+$route = request()->route()->uri; // haal de huidige URI op: zoals login of register
 
 ?>
 <nav class="navbar navbar-expand-sm fixed-top">
 	<div class="container-fluid ">
-		<a class="navbar-brand">
+		<a class="navbar-brand" href="/">
 			<img src="../img/logo.gif" alt="JukeBox_logo" width="250">
 		</a>
-		@if (1==1)
+		@if ($route != "login" && $route != "register")
 			<div class="collapse navbar-collapse">
 				<ul class="navbar-nav ms-auto">
 					<li class="nav-item">
