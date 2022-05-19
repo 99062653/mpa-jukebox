d97939467d72c7738bdd2fda8ec9f1163dbb5970
99062653 <99062653@mydavinci.nl>
2022-05-19 13:31:29 +0200

    v0.41
    -fix time

--- resources/views/funcs/time.blade.php
+++ resources/views/funcs/time.blade.php
@@ -3,20 +3,20 @@
 use Carbon\Carbon;
 
 ?>
-@switch($t = date("h", strtotime(Carbon::now())))
-            @case($t >= 06 && $t < 12)
+@switch($t = date(now()->format('H:i:s')))
+            @case($t > 06 && $t < 12)
             <h1>Goedemorgen,</h1>
             @break
 
-            @case($t >= 12 && $t < 18)
+            @case($t > 12 && $t < 18)
             <h1>Goedemiddag,</h1>
             @break
 
-            @case($t >= 18 && $t < 24)
+            @case($t > 18 && $t < 24)
             <h1>Goedeavond,</h1>
             @break
 
-            @case($t >= 00 && $t < 06)
+            @case($t > 00 && $t < 06)
             <h1>Goedenacht,</h1>
             @break
 
