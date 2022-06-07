d6c6c49a568526c9d25eefd8d42372841924a1c6
99062653 <99062653@mydavinci.nl>
2022-06-08 00:04:15 +0200

    v0.45.4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,13 +20,15 @@ use App\Models\Genre;
             </div>
         </div>
         <div id="content-mid">
-            @foreach (Genre::all() as $Genre)
-            <div style="background-color: <?= $Genre->rgb_color ?>">
-                <b>
-                    <?= $Genre->name ?>
-                </b>
+            <div id="genres">
+                @foreach (Genre::all() as $Genre)
+                <div class="genre" style="background-color: <?= $Genre->rgb_color ?>">
+                    <b>
+                        <?= $Genre->name ?>
+                    </b>
+                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
         <div id="content-bottom">
 
