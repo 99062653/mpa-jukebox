37b9ba83a600043f619b014381a652a0d7e1bd81
99062653 <99062653@mydavinci.nl>
2022-06-07 10:27:36 +0200

    v0.45.1

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -27,10 +27,10 @@ class EntryController extends Controller
                 session()->put('user_id', $user->id);
                 return redirect('/');
             } else {
-                return view('login', ['issue' => 'Dit wachtwoord is niet juist', 'username' => $req->username]);
+                return view('entry/login', ['issue' => 'Dit wachtwoord is niet juist', 'username' => $req->username]);
             }
         } else {
-            return view('login', ['issue' => 'Dit account bestaat niet', 'username' => $req->username]);
+            return view('entry/login', ['issue' => 'Dit account bestaat niet', 'username' => $req->username]);
         }
     }
 
@@ -46,7 +46,7 @@ class EntryController extends Controller
             session()->put('user_id', $user->id);
             return redirect('/');
         } else {
-            return view('register', ['issue' => 'Dit account bestaat al', 'username' => $req->username]);
+            return view('entry/register', ['issue' => 'Dit account bestaat al', 'username' => $req->username]);
         }
     }
 
