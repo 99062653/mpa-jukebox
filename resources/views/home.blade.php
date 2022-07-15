553c75eb4c4da0a9dc9caa052584dc6cfe50b2b1
99062653 <99062653@mydavinci.nl>
2022-07-15 23:49:55 +0200

    v0.95.6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,14 +11,6 @@
     <div id="content">
         <div id="content-top">
             @include("funcs/time")
-            {{-- <div class="search-container">
-                <form action="#">
-                    <div class="form-group">
-                        <input type="text" class="form-control searchbar" placeholder="Search.." name="search">
-                        <button type="submit" class="searchbar-link"><i class="bi bi-search"></i></button>
-                    </div>
-                </form>
-            </div> --}}
         </div>
         <div id="content-mid">
                 <h2 id="h2-genres">Interessante Genres<span class="span-link"><a class="hidden-link" href="/genres">Alle</a></span></h2>
