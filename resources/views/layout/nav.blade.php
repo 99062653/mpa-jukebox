36b6892c43ada172b802d748addd915d2397e68e
Schwa <99062653@mydavinci.nl>
2022-08-23 18:35:57 +0200

    Revert "v0.95.6"
    This reverts commit 553c75eb4c4da0a9dc9caa052584dc6cfe50b2b1.

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -6,30 +6,32 @@
 		<div class="collapse navbar-collapse">
 			<ul class="navbar-nav ms-auto">
 				@if (!session('user_id'))
-					<li class="nav-item">
-						<a class="nav-link" href="/user/login">Login</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="/user/register">Register</a>
-					</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/user/login">Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/user/register">Register</a>
+				</li>
 				@else
-					<li class="nav-item">
-						<div class="dropdown">
-							<a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-								data-bs-toggle="dropdown" aria-expanded="false">
-								<i class="bi bi-person-circle"></i>
-							</a>
-			
-							<ul class="dropdown-menu" id="dropdown-list" aria-labelledby="dropdownMenuLink">
-								<li><a class="dropdown-item" href="/user"><i
-											class="bi bi-person-circle dropdown-icon"></i>Account</a></li>
-								@if (session('user_admin'))
-									<li><a class="dropdown-item" href="/admin/panel"><i class="bi bi-webcam dropdown-icon"></i>Panel</a></li>
-								@endif
-								<li><a class="dropdown-item" href="/user/logout"><i class="bi bi-box-arrow-left dropdown-icon"></i>Logout</a></li>
-							</ul>
-						</div>
-					</li>
+				<li class="nav-item">
+					<div class="dropdown">
+						<a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+							data-bs-toggle="dropdown" aria-expanded="false">
+							<i class="bi bi-person-circle"></i>
+						</a>
+		
+						<ul class="dropdown-menu" id="dropdown-list" aria-labelledby="dropdownMenuLink">
+							<li><a class="dropdown-item" href="/user"><i
+										class="bi bi-person-circle dropdown-icon"></i>Account</a></li>
+							@if (session('user_admin'))
+							<li><a class="dropdown-item" href="/admin/panel"><i class="bi bi-webcam dropdown-icon"></i>Panel</a>
+							</li>
+							@endif
+							<li><a class="dropdown-item" href="/user/logout"><i
+										class="bi bi-box-arrow-left dropdown-icon"></i>Logout</a></li>
+						</ul>
+					</div>
+				</li>
 				@endif
 			</ul>
 		</div>
